99c59c22d0937a35b046e96adf7e2ff1fd505813
Siegfried <bachbrahms@163.com>
2020-08-15 17:04:05 +0800

    addition

--- src/main/java/com/gengyu/msgnotification/config/ResourceMappingConfig.java
+++ src/main/java/com/gengyu/msgnotification/config/ResourceMappingConfig.java
@@ -1,21 +1,17 @@
 package com.gengyu.msgnotification.config;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Slf4j
 @Configuration
 public class ResourceMappingConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // 将本地资源路径映射为网络路径
-        //Windows下
+        //Windows下  此时前端访问路径：http://xx.xx.xx.xx:xx/uploads/文件名
         registry.addResourceHandler("/uploads/**").addResourceLocations("file:D:/codes/message_notification/upload/");
-        // 前端访问路径：http://xx.xx.xx.xx:xx/uploads/文件名
         //Mac或Linux下(没有CDEF盘符)
 //        registry.addResourceHandler("/uploads/**").addResourceLocations("file:/Users/uploads/");
-//        super.addResourceHandlers(registry);
     }
 }
